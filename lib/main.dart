e25805b8951b8215f6a6866975e396b1f89c0225
Ngoufack Edgard <ngoufackedgard1@gmail.com>
2024-04-06 15:08:28 +0100

    design finished

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 
 void main() {
   runApp(const MyApp());
@@ -10,7 +12,7 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return GetMaterialApp(
       title: 'Todo app',
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
@@ -38,11 +40,12 @@ class _MyHomePageState extends State<MyHomePage> {
     debugPrint(_textController.text);
   }
 
-@override
+  @override
   void initState() {
     _textController = TextEditingController();
     super.initState();
   }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -52,6 +55,9 @@ class _MyHomePageState extends State<MyHomePage> {
           widget.title,
           style: TextStyle(fontWeight: FontWeight.bold, fontSize: 24),
         ),
+        actions: [
+          IconButton(onPressed: (){}, icon: Icon(CupertinoIcons.heart_fill,color: Colors.red,))
+        ],
       ),
       body: Padding(
         padding: const EdgeInsets.all(20.0),
@@ -68,22 +74,115 @@ class _MyHomePageState extends State<MyHomePage> {
                   contentPadding: EdgeInsets.all(15)),
               textCapitalization: TextCapitalization.sentences,
             ),
-            SizedBox(height: 20,),
+            SizedBox(
+              height: 20,
+            ),
             Expanded(
                 child: ListView.separated(
-                    itemBuilder: (ctx, index) => ListTile(
-                          title: Text("Item $index"),
-                          trailing: Icon(Icons.more_vert_rounded),
+                    itemBuilder: (ctx, index) => Container(
+                          padding: EdgeInsets.all(8),
+                          decoration: BoxDecoration(
+                              color: Colors.grey.shade200,
+                              borderRadius:
+                                  BorderRadius.all(Radius.circular(15))),
+                          child: InkWell(
+                            onTap: () {},
+                            child: Column(
+                              mainAxisAlignment: MainAxisAlignment.start,
+                              mainAxisSize: MainAxisSize.min,
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                ListTile(
+                                  title: Text("Note ${index+1}",style: TextStyle(fontWeight: FontWeight.bold),),
+                                  trailing: Icon(Icons.more_vert_rounded),
+                                  contentPadding: EdgeInsets.all(5),
+                                ),
+                                Text("27 janvier 2024",style: TextStyle(color: Colors.grey),)
+                              ],
+                            ),
+                          ),
                         ),
-                    separatorBuilder: (ctx, index) => Divider(),
+                    separatorBuilder: (ctx, index) => SizedBox(
+                          height: 10,
+                        ),
                     itemCount: 15))
           ],
         ),
       ),
       floatingActionButton: FloatingActionButton(
-        onPressed: submitText,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
+        onPressed: () async {
+          await showDialog(
+              context: context,
+              builder: (ctx) {
+                return AlertDialog(
+                  actionsAlignment: MainAxisAlignment.spaceBetween,
+                  title: Text(
+                    "Create note",
+                    style: TextStyle(fontWeight: FontWeight.bold),
+                  ),
+                  content: Column(
+                    mainAxisSize: MainAxisSize.min,
+                    children: [
+                      TextField(
+                        decoration: InputDecoration(
+                            border: OutlineInputBorder(
+                                borderRadius:
+                                    BorderRadius.all(Radius.circular(15))),
+                            label: Text("Name"),
+                            hintText: "Enter the title",
+                            contentPadding: EdgeInsets.all(15)),
+                        textCapitalization: TextCapitalization.sentences,
+                      ),
+                      SizedBox(
+                        height: 10,
+                      ),
+                      TextField(
+                        maxLines: 3,
+                        decoration: InputDecoration(
+                            border: OutlineInputBorder(
+                                borderRadius:
+                                    BorderRadius.all(Radius.circular(15))),
+                            label: Text("Description"),
+                            hintText: "Enter the description of note",
+                            contentPadding: EdgeInsets.all(15)),
+                        textCapitalization: TextCapitalization.sentences,
+                      ),
+                    ],
+                  ),
+                  actions: [
+                    MaterialButton(
+                      minWidth: 100,
+                      color: Colors.red,
+                      height: 47,
+                      shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.all(Radius.circular(18))),
+                      onPressed: () {
+                        Get.back();
+                      },
+                      child: Text(
+                        "Cancel",
+                        style: TextStyle(color: Colors.white),
+                      ),
+                    ),
+                    MaterialButton(
+                      minWidth: 100,
+                      color: Colors.indigo,
+                      height: 47,
+                      onPressed: () {},
+                      child: Text(
+                        "Ok",
+                        style: TextStyle(color: Colors.white),
+                      ),
+                      shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.all(Radius.circular(18))),
+                    )
+                  ],
+                  //        icon: Icon(Icons.new_label),
+                );
+              });
+        },
+        tooltip: 'New note',
+        child: const Icon(Icons.add_rounded),
       ),
     );
   }
